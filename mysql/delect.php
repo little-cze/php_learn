2b5747ea57bdfca71f4ce28f768a94f5a9fcc010
cze <1750456942@qq.com>
2020-10-17 09:54:13 +0800

    'commit'

--- mysql/delect.php
+++ mysql/delect.php
@@ -1,1 +1,28 @@
 <?php
+
+///删除
+///
+$con = mysqli_connect("localhost","root","","myDB");
+if(mysqli_connect_error()){
+    die("出错了铁汁");
+}
+$del = "delete from mytable where firstname='mike'";
+if($con->query($del) === TRUE){
+    echo "删除成功" . "<br>";
+}else{
+    echo "删除出错啦铁汁@" . $con->error;
+}
+mysqli_query($con,"delete from mytable where firstname='mike'");
+
+$sql = "select * from mytable";
+$result = $con->query($sql);
+if($result->num_rows>0){
+    while($row = $result->fetch_assoc()){
+        echo $row["id"]."->". $row['firstname'] . $row["email"] ."<br>";
+    }
+}else{
+    echo "数量为0";
+}
+
+
+mysqli_close($con);
--- mysql/upload.php
+++ mysql/upload.php
@@ -1,1 +1,22 @@
 <?php
+
+///更新
+///
+$con = mysqli_connect("localhost","root","","myDB");
+if(mysqli_connect_error()){
+    echo "出错了".mysqli_connect_error();
+}
+$sql = "update mytable set name=cze where first id = 1";
+mysqli_query($con,$sql);
+
+$sel = "select * from mytable";
+$result = $con->query($sel);
+if($result-> num_rows> 0){
+    while($row=$result->fetch_assoc()){
+        echo $row["firstname"] . "<br>";
+    }
+}else{
+    echo "Error:出错了铁汁";
+}
+
+mysqli_close($con);
